896a9457209f4c6e4ab06fc300f8e09a584af137
Marvin Bruns <android.bruns@googlemail.com>
2018-10-09 17:58:17 +0200

    flatten

--- KE1.pl
+++ KE1.pl
@@ -7,7 +7,15 @@ ue_member(Element, [Element|_]).
 ue_member(Element, [H|R]) :- ue_member(Element, R).
 
 % 3.3
+% \+(T) :- call(T), !, fail.
+% \+(_).
+
+connect([], L2, L2).
+connect([L1H|L1T], L2, [L1H|E]) :- connect(L1T, L2, E).
+
 flatten([], []).
+flatten([InHead|InTail], [InHead|Out]) :- flatten(InTail, Out), \+(is_list(InHead)).
+flatten([InHead|InTail], Out) :- flatten(InHead, OutHead), flatten(InTail, OutTail), connect(OutHead, OutTail, Out).
 
 % 3.4
 ue_length([], 0).
