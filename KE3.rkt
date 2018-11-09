a8fc72a4dad596c2265c389a42f524f97f5d211b
Marvin Bruns <android.bruns@googlemail.com>
2018-11-09 20:17:55 +0100

    Aufgabe 2.1 rdy

--- KE3.rkt
+++ KE3.rkt
@@ -1,3 +1,4 @@
+;; Aufgabe 1
 (define (insert-sort liste)
 
   (define (insert element liste)
@@ -18,6 +19,28 @@
   )
 )
 
-; (insert-sort '(2 4 1 6 4))
-; (insert-sort '())
-; (insert-sort '(3 2 1))
+#|
+(insert-sort '(2 4 1 6 4))
+(insert-sort '())
+(insert-sort '(3 2 1))
+|#
+
+
+;; Aufgabe 2.1
+(define (ableiten fkt k)
+  (lambda(x)
+     (/
+      (- (fkt (+ x k))  (fkt (- x k)))
+      (* 2 k)
+     )
+  )
+)
+
+#|
+(define (sqr x) (* x x))
+(define ablsqr (ableiten sqr 0.1))
+ablsqr
+(ablsqr 1)
+(ablsqr 2)
+(ablsqr 3)
+|#
