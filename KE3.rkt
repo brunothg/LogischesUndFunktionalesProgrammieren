709ecb5f479cff6850742e12cbf79dd535b5fdc3
Marvin Bruns <android.bruns@googlemail.com>
2018-11-09 18:46:13 +0100

    KE3 Aufgabe 1

---
+++ KE3.rkt
@@ -0,0 +1,23 @@
+(define (insert-sort liste)
+
+  (define (insert element liste)
+    (if (null? liste)
+        (list element)
+        (let ((firstListElement (car liste)) (restListe (cdr liste)))
+          (if (< element firstListElement)
+              (cons element liste)
+              (cons firstListElement (insert element restListe))
+          )
+        )
+    )
+  )
+  
+  (if (null? liste)
+    '()
+    (insert (car liste) (insert-sort (cdr liste)))
+  )
+)
+
+; (insert-sort '(2 4 1 6 4))
+; (insert-sort '())
+; (insert-sort '(3 2 1))
