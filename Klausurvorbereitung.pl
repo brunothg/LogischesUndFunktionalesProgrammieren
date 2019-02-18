d810fed2ef119df980920253b410072b2f5171d4
Marvin Bruns <android.bruns@googlemail.com>
2019-02-18 18:48:47 +0100

    Übungsaufgaben

---
+++ Klausurvorbereitung.pl
@@ -0,0 +1,25 @@
+% Übungsaufgaben aus den Kurseinheiten und den Klausurprotokollen
+
+% member/2 - Prüfe, ob ein Element in einer Liste enthalten ist
+
+% last/2 - Prüfe, ob ein Element das letzte Element einer Liste ist
+
+% is_list/1 - Prüfe ob ein Element eine Liste ist
+
+% reverse/2 - Invertiere eine gegebene Liste
+
+% sortiert/1 - Prüfe, ob eine Liste sortiert ist
+
+% append/3 - Verbinde erste beiden Listen zu dritter
+
+% member/2 - Variation mit append/3
+
+% last/2 - Variation mit append/3
+
+% ith/3 - Prüft, ob das erste Argument an der Stelle von N in der Liste L steht
+
+% if-then-else mit Cut-Operator
+
+% length/2 Länge einer Liste
+
+% alternative Darstellung für \+ (NOT)
---
+++ Klausurvorbereitung.rkt
@@ -0,0 +1,39 @@
+(load "k1816-extensions.scm")
+:- use_module(library(clpfd)).
+;; Übungsaufgaben aus den Kurseinheiten und den Klausurprotokollen
+
+;; average - Berechnung des Durchschnitts zweier Zahlen
+
+;; abs - Berechnung des Absolutbetrags einer Zahl
+
+;; atom? - Testet, ob das ihr übergebene Argument ein Atom ist (mit cond)
+
+;; atom? - Variation mit OR
+
+;; ith - Liefert das i-te Element einer Liste zurück
+
+;; length - Berechnet die Länge einer Liste
+
+;; append - Listenkonkatenation
+
+;; produkt - Gib das Produkt einer Liste von Zahlen zurück
+
+;; sum-with - Eine Funktion (Summe von bis), die eine andere Funktion zurückgibt
+
+;; dreimal - Gibt eine Funktion zurück, die die übergebene Funktion dreimal anwendet.
+
+;; compose - ein Programm, dass die Komposition zweier Funktionen darstellt
+
+;; n-mal - eine Funktion, die eine Funktion n-mal ausführt
+
+;; filter - eine Funktion, die eine Liste und ein Prädikat als Eingabe erwartet und eine Liste der Elemente aus der Eingabeliste zurückgibt, die das Prädikat erfüllen
+
+;; insert - Fügt ein Element sortiert in eine Liste ein
+
+;; gerade? - Prüft, ob ein Element in einer Liste enthalten ist
+
+;; last - Überprüft, ob ein Element das letzte Element einer Liste ist
+
+;; right reduce Funktion für Listenkonkatenation
+
+;; sum-stream - Berechnet die Summe aller Elemente eines Stroms von Zahlen
