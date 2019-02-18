132887196cffd0511c8782ce4ec58d255e65bb2e
Marvin Bruns <android.bruns@googlemail.com>
2019-02-18 19:32:40 +0100

    Übungsaufgaben

--- Klausurvorbereitung.pl
+++ Klausurvorbereitung.pl
@@ -2,9 +2,12 @@
 
 % member/2 - Prüfe, ob ein Element in einer Liste enthalten ist
 
+
 % last/2 - Prüfe, ob ein Element das letzte Element einer Liste ist
 
 % is_list/1 - Prüfe ob ein Element eine Liste ist
+ue_is_list([]).
+ue_is_list([_|R]) :- ue_is_list(R).
 
 % reverse/2 - Invertiere eine gegebene Liste
 
@@ -19,7 +22,13 @@
 % ith/3 - Prüft, ob das erste Argument an der Stelle von N in der Liste L steht
 
 % if-then-else mit Cut-Operator
+ite(I, T, E) :- call(I), T, !, true.
+ite(I, T, E) :- call(E).
 
 % length/2 Länge einer Liste
+ue_length([], 1).
+ue_length([_|R], N) :- ue_length(R, N1), N is N1 + 1.
 
 % alternative Darstellung für \+ (NOT)
+not(X) :- call(X), !, fail.
+not(X) :- true.
